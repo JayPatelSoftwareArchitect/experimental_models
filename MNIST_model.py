b69873803ffba7934b36478b11f36cf2b355b718
Jay Patel <JayPatelSoftwareArchitect@github.com>
2021-04-17 15:30:38 -0500

    save models

--- MNIST_model.py
+++ MNIST_model.py
@@ -4,6 +4,7 @@ import tensorflow.keras
 import matplotlib.pyplot as plt
 from tensorflow.keras import layers
 import pathlib
+import os
 from NonLinearActivations import CustomNonlinearActivation
 #This code snipet is for testing different non linear activation functions, and custom complex activation functions.
 #For first training iteration, It will use default activation functions like relu and softmax, after that it will use my custom activation functions.
@@ -19,8 +20,7 @@ x_train, x_test = x_train/255.0, x_test/255.0
 print("Number of original training examples:", len(x_train))
 print("Number of original test examples:", len(x_test))
 
-#A dynamic model test.
-def test_model(activationfn,epochs=2):
+def create_model(activationfn):
     #sequential api
     model = tf.keras.Sequential([
         #Dense layer 1
@@ -32,16 +32,26 @@ def test_model(activationfn,epochs=2):
         #Flatten the last layer input
         tf.keras.layers.Flatten(),
         #apply output layer, Here we have 10 output labels ranging from 0-9 
-        tf.keras.layers.Dense(units=10),
+        tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax),
     ])
     model.compile(
         optimizer='adam',
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy']
     )
+    return model
 
+#A dynamic model test.
+def test_model(activationfn,epochs=2):
+    #create model with given activation function
+    model = create_model(activationfn)
     model.fit(x_train, y_train, epochs=epochs)
     model.evaluate(x_test,y_test)
+    return model
+
+checkpoint_path = "trained_models/"
+#this activations will be tested with a simple dnn.
+activation_names = ['relu','add_c3','sub_c2','mul_c2','div_c3']
 
 # A helper function to iterate through activation functions.
 def testActivations(epochs=2):
@@ -50,12 +60,11 @@ def testActivations(epochs=2):
     activation_dict = [
                     #relu, only account values greater then 0.
                     tf.keras.activations.relu,
-                    #softmax, probabilistic activation
-                    tf.keras.activations.softmax,
+
                     #following are custom activation functions
-                    #This functions performs well with almost same accuracy as relu
                     #log(1+a) + e^|ib| 
                     custom_functions.getActivationFunction('add_c3'),
+                    #This functions performs well with almost same accuracy as relu
                     #e^a - e^|ib|
                     custom_functions.getActivationFunction('sub_c2'),
                     #e^a * e^|ib|
@@ -65,8 +74,13 @@ def testActivations(epochs=2):
                     ]
     
     for index in range(len(activation_dict)):
-        test_model(activation_dict[index], epochs=epochs)
-
-#call dynamic model tests, here testactivations function will go through few activation functions and it will train a sample model. 
+        save_model_path = checkpoint_path+"/model_trained_by_"+activation_names[index]+"/"
+        checkpoint_dir = os.path.dirname(save_model_path)
+        #train model with different activation functions
+        trained_model = test_model(activation_dict[index], epochs=epochs)
+        trained_model.save(checkpoint_dir)
+#call dynamic model tests, here testactivations function will go through few activation functions and it will train a sample model and will save it in trained_models dir. 
 testActivations(epochs=10)
+
+
 
